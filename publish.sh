295bb6437cf3f9fab4803ac014dea681a09a6d7f
GGG <gggkiller2@gmail.com>
2022-10-10 01:30:43 -0300

    Publish archives to its own subdir.
    Publishing to .publish was being annoying as dragging doesn't work and the file browser won't show hidden files.

--- publish.sh
+++ publish.sh
@@ -3,13 +3,13 @@
 # shellcheck shell=bash
 set -eu
 
-rm -r .publish/
-mkdir -p .publish/archives/
+rm -r .publish/ archives/ || true
+mkdir archives/
 
 for rid in "win-x64" "win-arm64" "linux-x64" "linux-arm64" "osx-x64" "osx-arm64"; do
     echo Publishing $rid version...
     if dotnet publish --runtime "$rid" --configuration Release --output ".publish/$rid/" --self-contained src/CLI/LuaPack.CLI.csproj; then
-        zip -r ".publish/archives/$rid.zip" ".publish/$rid/"
-        tar czf ".publish/archives/$rid.tar.gz" ".publish/$rid/"
+        zip -r "archives/$rid.zip" ".publish/$rid/"
+        tar czf "archives/$rid.tar.gz" ".publish/$rid/"
     fi
 done
